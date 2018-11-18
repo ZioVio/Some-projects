49b32404131eb2373f1bdfb46fed8a0b39e03421
Андрей <anderkoval3@gmail.com>
2018-11-18 14:18:28 +0200

    test commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+#include <math.h>
+#include <stdlib.h>
+#include <progbase.h>
+#include <stdbool.h>
+#include <progbase/console.h>
+
+int main(int argc, char *argv[]) {
+    
+    return 0;
+}
