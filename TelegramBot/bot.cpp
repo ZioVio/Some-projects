64296b38afe0b4a3af7bfb6769ef81b880775b9e
Андрей <anderkoval3@gmail.com>
2019-04-25 19:07:44 +0300

    commands from console are working!

--- TelegramBot/bot.cpp
+++ TelegramBot/bot.cpp
@@ -1,34 +1,46 @@
-#include <cstdio>
+// #include <cstdio>
 #include <tgbot/tgbot.h>
 #include <string> 
 #include <iostream>
 #include <progbase/console.h>  
 
+
+
 int main()
-{
-    std::string console_msg;
-    TgBot::Message::Ptr g_message;
-    getline(std::cin, console_msg);     
+{     
+    int id = 0;
     TgBot::Bot bot("624807853:AAFpoKOjWkpGqXcbtCdhRAoORvllck13X8A");
-    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
-        bot.getApi().sendMessage(message->chat->id, "Hi!");
-    });//@todo learn this shit
-    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
-        printf("User wrote %s\n", message->text.c_str());
-        if (StringTools::startsWith(message->text, "/start"))
-        {
-            return;
-        }
-        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+    bot.getEvents().onCommand("start", [&bot, &id](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Hi!"); 
+        id = message->chat->id; 
+    });
+    bot.getEvents().onAnyMessage([&bot, &id](TgBot::Message::Ptr message) {
+        // printf("User wrote %s\n", message->text.c_str());
+        // if (StringTools::startsWith(message->text, "/start"))
+        // {
+        //     return;
+        // }
+        std::string msg; 
+        id = message->chat->id;
+        getline(std::cin, msg);  
+        bot.getApi().sendMessage(message->chat->id, "Your message is: " + msg);
     });
     try
     {
-        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        // printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
         TgBot::TgLongPoll longPoll(bot);
         while (true)
         {
             printf("Long poll started\n");
-            longPoll.start();            
+            longPoll.start();                        
+            if (id != 0)
+                break;
+        } 
+        while(true) {
+            std::string msg;
+            getline(std::cin, msg); 
+            bot.getApi().sendMessage(id, "Your message is: " + msg);
+
         }
     }
     catch (TgBot::TgException &e)
